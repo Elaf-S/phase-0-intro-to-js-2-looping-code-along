8d2e15487652d246cac9b5948f07a72a2f5770ca
elaf swara <elafswara4@gmail.com>
2022-08-13 01:34:04 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(name,event){
+    const array=[];
+ for (let i=0; i<name.length; i++){
+    array.push( `Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+ }
+ return array;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+ 
+function  countDown(num){
+ let i=num;
+ while(i>=0){
+    console.log(i);
+    i--;
+ }
+ return num;
+}
